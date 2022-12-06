2c0e896e7330f965abd81665fcf2f2e666a421a4
Alex C. Kraasch <git.global@kraasch.eu>
2022-12-06 13:59:42 +0100

    Update solution 1.

--- day01/solution.py
+++ day01/solution.py
@@ -1,12 +1,32 @@
 
 import os
+import queue
+
+q = queue.Queue(maxsize=3) # queue of max_ids
+class queue_of_three:
+    xs = []
+    def __init__(self):
+        self.xs = []
+    def put(self, x):
+        if len(self.xs) == 3:
+            self.xs[0] = self.xs[1]
+            self.xs[1] = self.xs[2]
+            self.xs[2] = x
+        else:
+            self.xs.append(x)
+    def get(self):
+        return self.xs
 
 NL = os.linesep # new line characters.
 nl_len = len(NL) # length of NL characters.
 s = 0 # sum.
 m = 0 # max.
 i = 1 # elf ID.
-max_id = 0 # elf ID associated with the max.
+
+# replaced by queue.
+q_max = queue_of_three()
+q_ids = queue_of_three()
+
 with open("input.txt") as myfile:
     for line in myfile:
         # end the current addition.
@@ -14,7 +34,11 @@ with open("input.txt") as myfile:
             # look whether the current sum is a new max.
             if s > m:
                 m = s
-                max_id = i
+
+                # replaced by queue.
+                q_max.put(m)
+                q_ids.put(i)
+
             s = 0 # resset the current sum.
             i = i + 1 # increment elf ID.
             continue
@@ -23,5 +47,8 @@ with open("input.txt") as myfile:
         num_int = int(num_str)
         s = s + num_int
 
-print(f"Elf #{max_id} has {m} callories")
+max_ids   = q_ids.get()
+max_cals  = q_max.get()
+total_cal = sum(max_cals)
+print(f"Elves with most calories are #{max_ids}, they have {total_cal} callories ({max_cals}).")
 
---
+++ day01/tests/test_solution.py
@@ -0,0 +1,15 @@
+
+import pytest
+
+import solution as SLN
+
+# TODO: abstract from input files and text strings.
+# read line from that abstraction. 
+# make test use strings, make implementation use files.
+
+# TODO: import some testable module from the solution
+# and test that module here.
+
+def test_findmax():
+    assert 1 == 1
+
